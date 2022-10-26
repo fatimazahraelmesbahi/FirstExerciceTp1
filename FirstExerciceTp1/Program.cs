dc115bc06bd09185221ae395275f3a76f784f840
HP <HP@DESKTOP-RG3QUVA>
2022-10-26 21:19:38 +0200

    Add project files.

---
+++ FirstExerciceTp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstExerciceTp1
+{
+    internal class Program
+    {
+        public static bool EstPremier(int premier)
+        {
+            if (premier < 2)
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = 2; i < premier; i++)
+                {
+                    if (premier % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+            static void Main(string[] args)
+        {
+            int premier = 0;
+            do
+            {
+                Console.WriteLine("entered a number :");
+
+            } while (!int.TryParse(Console.ReadLine(), out premier));
+
+            if (EstPremier(premier))
+            {
+                Console.WriteLine("est premier");
+            }
+            else
+            {
+                Console.WriteLine("Pas premier");
+            }
+        }
+    }
+}
